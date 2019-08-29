cae42b20bac10549c4f4a23908bcc0e7001d5c8b
Afterimage Designs <hello@afterimagedesigns.com>
2019-08-29 14:05:35 +0800

    Add files via upload

---
+++ inc/plugin-compatibility/plugin-compatibility.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/plugin-compatibility/jetpack/jetpack.php';
+
+/**
+ * Load Woocommerce compatibility file.
+ */
+require get_template_directory() . '/inc/plugin-compatibility/woocommerce/woocommerce.php';
